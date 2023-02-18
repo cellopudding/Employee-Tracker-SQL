fef9c09c337c656d1565f76171236ec21b746c64
Lonnie Root <root.lonnie@gmail.com>
2023-02-17 20:04:56 -0500

    few additions

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+// GIVEN a command-line application that accepts user input
+// WHEN I start the application
+// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+// WHEN I choose to view all departments
+// THEN I am presented with a formatted table showing department names and department ids
+// WHEN I choose to view all roles
+// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+// WHEN I choose to view all employees
+// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+// WHEN I choose to add a department
+// THEN I am prompted to enter the name of the department and that department is added to the database
+// WHEN I choose to add a role
+// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+// WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
 const fs = require('fs');
 const path = require('path')
-const inquirer = require('inquirer')
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
-const app = express();
+// const app = express();
+var figlet = require('figlet');
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// // Express middleware
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
@@ -19,5 +37,68 @@ const db = mysql.createConnection(
     password: 'password',
     database: 'employees_db'
   },
-  console.log(`Connected to the employees_db database.`)
+  console.log(`Welcome to the employees_db database!`)
 );
+letsBegin();
+
+
+//LETS BEGIN
+
+
+function letsBegin() {
+  console.log(figlet.textSync('Employee Tracker', {
+    font: 'Standard',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}));
+  
+  inquirer.prompt({
+      
+      type: "list",
+      choices: [
+        "Add department",
+        "Add role",
+        "Add employee",
+        "View departments",
+        "View roles",
+        "View employees",
+        "Update employee role",
+        "Quit"
+      ],
+      message: "What would you like to do?",
+      name: "option"
+    })
+    .then(function(result) {
+      console.log("You entered: " + result.option);
+
+      switch (result.option) {
+        case "Add department":
+          addDepartment();
+          break;
+        case "Add role":
+          addRole();
+          break;
+        case "Add employee":
+          addEmployee();
+          break;
+        case "View departments":
+          viewDepartment();
+          break;
+        case "View roles":
+          viewRoles();
+          break;
+        case "View employees":
+          viewEmployees();
+          break;
+        case "Update employee role":
+          updateEmployee();
+          break;
+        default:
+          quit();
+      }
+    });
+}
+
+
