cf684f890352097131d80dbac5138f8a9a1ab074
Lonnie Root <root.lonnie@gmail.com>
2023-02-17 19:08:46 -0500

    first push

---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roll (
+  id INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id),
+  REFERENCES department(id) 
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  roll_id INT NOT NULL,
+  manager_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (manager_id),
+  REFERENCES employee(id)
+
+);
+
