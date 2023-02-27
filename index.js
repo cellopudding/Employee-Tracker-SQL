364da71aad9cd3dbf484e267caa2853a46f587d8
Lonnie Root <root.lonnie@gmail.com>
2023-02-27 13:25:49 -0500

    two new functions

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function letsBegin() {
 
 // WHEN I choose to view all departments
 // THEN I am presented with a formatted table showing department names and department ids
-letsBegin();
+letsBegin()
 const viewDepartments = ()=> {
   db.query(`SELECT * FROM department`, function(err, results){
     console.log(`\n`)
@@ -115,8 +115,20 @@ const viewEmployees = ()=> {
 
 // WHEN I choose to add a department
 // THEN I am prompted to enter the name of the department and that department is added to the database
-
-
+function addDepartment() {
+  inquirer.prompt([
+    {
+      name: "department_name",
+      message: "What department would you like to add?"
+    }
+  ])
+  .then(results => {
+    let departmentName = results
+    db.promise().query("INSERT INTO department SET ?", departmentName)
+    .then(()=> console.log(`added ${departmentName.department_name}`))
+    .then(()=> letsBegin())
+  })
+}
 
 
 
@@ -124,41 +136,118 @@ const viewEmployees = ()=> {
 // WHEN I choose to add a role
 // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 function addRole() {
+  db.query(`SELECT * FROM role`, function(err, results){
+    let department = results
+    const departmentChoices = department.map(({id, name}) => ({
+      name: name,
+      value: id
+    }))
+  
   inquirer.prompt([
       {
         type: "input",
         message: "What is the role name?",
-        name: "whatRole"
+        name: "title"
       },
       {
         type: "input",
         message: "What is the salary for this role?",
-        name: "whatSalary"
+        name: "salary"
       },
       {
         type: "input",
         message: "What is the department id number?",
-        name: "whatDept"
+        name: "department_id",
+        choices: departmentChoices
       }
     ])
-  .then(function(answer) {
-  db.query("INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)", [answer.whatRole, answer.whatSalary, answer.whatDept], function(err, results) {
-        // if (err) throw err
-        console.table(results)
-        letsBegin()
-      })
-    })
+  
+  .then(answer => {
+    db.promise().query("INSERT INTO role SET ?", answer)
+    .then(()=> console.log(`added ${answer.title}`))
+    .then(()=> letsBegin())
+  }) 
+  
+})
 }
 
 
 // WHEN I choose to add an employee
 // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+function addEmployee() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the employees first name?",
+      name: "first_name"
+    },
+    {
+      type: "input",
+      message: "What is the employees last name?",
+      name: "last_name"
+    }
+  ])
 
+  .then(results => {
+    let firstName = results.first_name
+    let lastName = results.last_name
+  
+    db.promise().query("SELECT role.id, role.title, department.id AS department, role.salary FROM role LEFT JOIN department on role.department_id = department.id;")
+    .then(([rows])=> {
+      let roles = rows
+      const roleChoices = roles.map(({id, title})=> ({
+        name: title,
+        value: id
+    
+      }))
+      inquirer.prompt({
+        type: "list",
+        name: "role_id",
+        message: "What is the employees role?",
+        choices: roleChoices
+      })
+      .then(results => {
+        let roleId = results.role_id
+        db.promise().query(`SELECT * FROM employee`)
+        .then(([rows])=> {
+          let employees = rows
+          const managerChoices = employees.map(({id, first_name, last_name}) => ({
+            name: `${first_name}${last_name}`, 
+            value: id
+          }))
+          managerChoices.unshift({
+            name: "none",
+            value: null
+          })
+          inquirer.prompt({
+            type: "list",
+            name: "managerId",
+            message: "Who is the employee's manager?",
+            choices: managerChoices
+          })
+            .then(res => {
+              let employee = {
+                manager_id: res.managerId,
+                role_id: roleId,
+                first_name: firstName,
+                last_name: lastName
+              }
+db.promise().query("INSTERT INTO employee SET ?", employee)
 
-
-
-
-
+        })
+        .then(() => console.log(
+          `Added ${firstName} ${lastName} to the database`
+        ))
+        .then(() => letsBegin())
+        
+      })
+    
+    })
+  
+  })
+  
+  }
+)}
 
 // WHEN I choose to update an employee role
 // THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
